83d089e471156868417b272b41c54ea3c5b6299e
thadh <thadh@THADH-MTV.ad.corp.google.com>
2009-03-08 22:20:30 -0700

    go

--- src/com/hughes/android/dictionary/Dictionary.java
+++ src/com/hughes/android/dictionary/Dictionary.java
@@ -1,61 +1,110 @@
 package com.hughes.android.dictionary;
 
-import java.io.File;
 import java.io.IOException;
 import java.io.RandomAccessFile;
+import java.util.ArrayList;
+import java.util.List;
 
-import com.hughes.util.FileUtil;
-import com.hughes.util.LRUCacheMap;
+import com.hughes.util.CachingList;
+import com.hughes.util.raf.FileList;
+import com.hughes.util.raf.RAFFactory;
+import com.hughes.util.raf.RAFSerializable;
+import com.hughes.util.raf.RAFSerializableSerializer;
+import com.hughes.util.raf.RAFSerializer;
 
-public final class Dictionary {
-
-  public static final String INDEX_FORMAT = "%s_index_%d";
-
-  private final byte lang;
-  private final File dictionaryFile;
-  private final RandomAccessFile dictionaryRaf;
-  private final Index index;
+public final class Dictionary implements RAFSerializable<Dictionary> {
   
-  private final LRUCacheMap<Integer,Row> positionToRow = new LRUCacheMap<Integer, Row>(2000);
+  static final RAFSerializer<Entry> ENTRY_SERIALIZER = new RAFSerializableSerializer<Entry>(Entry.RAF_FACTORY);
+  static final RAFSerializer<Row> ROW_SERIALIZER = new RAFSerializableSerializer<Row>(Row.RAF_FACTORY);
+  static final RAFSerializer<IndexEntry> INDEX_ENTRY_SERIALIZER = new RAFSerializableSerializer<IndexEntry>(IndexEntry.RAF_FACTORY);
 
-  public Dictionary(final String dictionaryFileName, final byte lang)
-      throws IOException {
-    this.lang = lang;
-    this.dictionaryFile = new File(dictionaryFileName);
+  final List<Entry> entries;
+  final Language[] languages = new Language[2];
+  
+  Language activeLanguage = null;
 
-    dictionaryRaf = new RandomAccessFile(dictionaryFile, "r");
-    index = new Index(String.format(INDEX_FORMAT, dictionaryFile
-        .getAbsolutePath(), lang));
+  public Dictionary(final String lang0, final String lang1) {
+    languages[0] = new Language(lang0);
+    languages[1] = new Language(lang1);
+    entries = new ArrayList<Entry>();
   }
   
-  public int numRows() {
-    return index.root.getDescendantCount();
+  public Dictionary(final RandomAccessFile raf) throws IOException {
+    entries = CachingList.create(FileList.create(raf, ENTRY_SERIALIZER, raf.getFilePointer()), 10000);
+    languages[0] = new Language(raf);
+    languages[1] = new Language(raf);
+  }
+  public void write(RandomAccessFile raf) throws IOException {
+    FileList.write(raf, entries, ENTRY_SERIALIZER);
+    languages[0].write(raf);
+    languages[1].write(raf);
   }
   
-  public Row getRow(final int position) throws IOException {
-    Row row = positionToRow.get(position);
-    if (row == null) {
-      final Object o = index.root.getDescendant(position);
-      if (o instanceof Integer) {
-        final Entry entry = new Entry(FileUtil.readLine(dictionaryRaf, (Integer) o));
-        row = new Row(entry.getFormattedEntry(lang), false);
-      } else if (o instanceof String) {
-        row = new Row((String) o, true);
-      } else {
-        throw new RuntimeException(o.toString());
-      }
-      positionToRow.put(position, row);
+  static final class Language implements RAFSerializable<Language> {
+    final String symbol;
+    final List<Row> rows;
+    final List<IndexEntry> sortedIndex;
+    
+    public Language(final String symbol) {
+      this.symbol = symbol;
+      rows = new ArrayList<Row>();
+      sortedIndex = new ArrayList<IndexEntry>();
     }
-    return row;
+
+    public Language(final RandomAccessFile raf) throws IOException {
+      symbol = raf.readUTF();
+      rows = CachingList.create(FileList.create(raf, ROW_SERIALIZER, raf.getFilePointer()), 10000);
+      sortedIndex = CachingList.create(FileList.create(raf, INDEX_ENTRY_SERIALIZER, raf.getFilePointer()), 10000);
+    }
+    public void write(final RandomAccessFile raf) throws IOException {
+      raf.writeUTF(symbol);
+      FileList.write(raf, rows, ROW_SERIALIZER);
+      FileList.write(raf, sortedIndex, INDEX_ENTRY_SERIALIZER);
+    }
   }
   
-  public static class Row {
-    final String text;
-    final boolean isWord;
-    public Row(final String text, final boolean isWord) {
-      this.text = text;
-      this.isWord = isWord;
+  public static final class Row implements RAFSerializable<Row> {
+    final int index;
+
+    public Row(int index) {
+      this.index = index;
     }
+
+    static final RAFFactory<Row> RAF_FACTORY = new RAFFactory<Row>() {
+      public Row create(RandomAccessFile raf) throws IOException {
+        return new Row(raf.readInt());
+      }};
+    public void write(RandomAccessFile raf) throws IOException {
+      raf.writeInt(index);
+    }
+  }
+
+  public static final class IndexEntry implements RAFSerializable<IndexEntry> {
+    final String word;
+    final int startRow;
+    
+    public IndexEntry(final String word, final int startRow) {
+      this.word = word;
+      this.startRow = startRow;
+    }
+
+    static final RAFFactory<IndexEntry> RAF_FACTORY = new RAFFactory<IndexEntry>() {
+      public IndexEntry create(RandomAccessFile raf) throws IOException {
+        final String word = raf.readUTF();
+        final int startRow = raf.readInt();
+        return new IndexEntry(word, startRow);
+      }};
+    public void write(final RandomAccessFile raf) throws IOException {
+      raf.writeUTF(word);
+      raf.writeInt(startRow);
+    }
+
+    @Override
+    public String toString() {
+      return word + "@" + startRow;
+    }
+    
+    
   }
 
 }
--- src/com/hughes/android/dictionary/Entry.java
+++ src/com/hughes/android/dictionary/Entry.java
@@ -1,33 +1,55 @@
 package com.hughes.android.dictionary;
 
+import java.io.IOException;
+import java.io.RandomAccessFile;
 import java.util.regex.Pattern;
 
-public final class Entry {
+import com.hughes.util.raf.RAFFactory;
+import com.hughes.util.raf.RAFSerializable;
+
+public final class Entry implements RAFSerializable<Entry> {
 
   static final byte LANG1 = 0;
   static final byte LANG2 = 1;
 
   static final Pattern lineSplitPattern = Pattern.compile("\\s+::\\s+");
 
-  String lang1 = "";
-  String lang2 = "";
-
-  Entry() {}
-
-  Entry(final String line) {
-    final boolean parsed = parseFromLine(line);
-    assert parsed;
-  }
-
-  boolean parseFromLine(final String line) {
-    final String[] parts = lineSplitPattern.split(line);
-    if (parts.length != 2) {
-      System.err.println("Entry:" + "Invalid line: " + line);
+  final String lang1;
+  final String lang2;
+  
+  @Override
+  public boolean equals(Object o) {
+    if (!(o instanceof Entry)) {
       return false;
     }
-    lang1 = parts[0];
-    lang2 = parts[1];
-    return true;
+    final Entry that = (Entry) o;
+    return that.lang1.equals(lang1) && that.lang2.equals(lang2);
+  }
+
+  @Override
+  public int hashCode() {
+    return lang1.hashCode() + lang2.hashCode();
+  }
+
+  @Override
+  public String toString() {
+    return getRawText();
+  }
+
+  public Entry(String lang1, String lang2) {
+    this.lang1 = lang1;
+    this.lang2 = lang2;
+  }
+
+  public static final RAFFactory<Entry> RAF_FACTORY = new RAFFactory<Entry>() {
+    public Entry create(RandomAccessFile raf) throws IOException {
+      final String lang1 = raf.readUTF();
+      final String lang2 = raf.readUTF();
+      return new Entry(lang1, lang2);
+    }};
+  public void write(RandomAccessFile raf) throws IOException {
+    raf.writeUTF(lang1);
+    raf.writeUTF(lang2);
   }
 
   String getAllText(final byte lang) {
@@ -56,6 +78,17 @@ public final class Entry {
 
   public String getRawText() {
     return getAllText(LANG1) + " :: " + getAllText(LANG2);
+  }
+  
+  
+
+  static Entry parseFromLine(final String line) {
+    final String[] parts = lineSplitPattern.split(line);
+    if (parts.length != 2) {
+      System.err.println("Entry:" + "Invalid line: " + line);
+      return null;
+    }
+    return new Entry(parts[0], parts[1]);
   }
 
 }
