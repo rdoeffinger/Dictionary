d835f0eb54cbf2eb092318d499772341c7331d93
Thad Hughes <thad.hughes@gmail.com>
2010-10-18 23:24:12 -0700

    go

---
+++ src/com/hughes/android/dictionary/C.java
@@ -0,0 +1,12 @@
+package com.hughes.android.dictionary;
+
+public class C {
+  
+  static final String DICTIONARY_CONFIGS = "dictionaryConfigs";
+
+  static final String DICT_INDEX = "dictIndex";
+  static final String INDEX_INDEX = "indexIndex";
+  static final String SEARCH_TOKEN = "searchToken";
+
+
+}
