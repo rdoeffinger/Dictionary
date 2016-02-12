509cd9e76f0d11b0ec6b7c4fc042d26c317ddf8b
Reimar Döffinger <Reimar.Doeffinger@gmx.de>
2016-02-12 21:57:55 +0100

    Remove checks that can never be true.

--- src/com/hughes/android/dictionary/DictionaryManagerActivity.java
+++ src/com/hughes/android/dictionary/DictionaryManagerActivity.java
@@ -590,10 +590,8 @@ public class DictionaryManagerActivity extends ActionBarActivity {
             builder.append(getString(R.string.indexInfo, indexInfo.shortName,
                     indexInfo.mainTokenCount));
         }
-        if (downloadable != null || dictionaryInfo != null) {
-            builder.append("; ");
-            builder.append(getString(R.string.downloadButton, (dictionaryInfo != null ? dictionaryInfo.uncompressedBytes : downloadable.uncompressedBytes) / 1024.0 / 1024.0));
-        }
+        builder.append("; ");
+        builder.append(getString(R.string.downloadButton, dictionaryInfo.uncompressedBytes / 1024.0 / 1024.0));
         if (broken) {
             name.setText("Broken: " + application.getDictionaryName(dictionaryInfo.uncompressedFilename));
             builder.append("; Cannot be used, redownload, check hardware/file system");
