bcc02126fe13685d0cf2885e9e94d9c18c77162e
Reimar Döffinger <Reimar.Doeffinger@gmx.de>
2017-11-06 21:30:48 +0100

    Update release script.
    In new gradle version, the output is in the
    release/ subdirectory.

--- release.sh
+++ release.sh
@@ -1,5 +1,5 @@
 VER=$(grep versionName AndroidManifest.xml | sed -e 's/.*"\(.*\)".*/v\1/')
 cp build/outputs/mapping/release/mapping.txt mapping-$VER.txt
-cp Dictionary-release.apk Dictionary-$VER.apk
+cp release/Dictionary-release.apk Dictionary-$VER.apk
 gpg -a --detach-sign Dictionary-$VER.apk
 git tag -u 06D4D9C7 "$VER"
