198b0bc69d5cb4c90c0c8f10b1ca072b44f17a3a
Reimar Döffinger <Reimar.Doeffinger@gmx.de>
2020-04-18 10:04:24 +0200

    Remove unused import.

--- src/com/hughes/android/dictionary/DictionaryApplication.java
+++ src/com/hughes/android/dictionary/DictionaryApplication.java
@@ -41,7 +41,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
