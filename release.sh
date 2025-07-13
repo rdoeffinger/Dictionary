c2053bcb7d239b7274368ae7f30e1e3937658c8a
Reimar Döffinger <Reimar.Doeffinger@gmx.de>
2025-07-13 23:38:29 +0200

    Also support app bundles in release script

--- release.sh
+++ release.sh
@@ -1,5 +1,6 @@
 VER=$(grep versionName AndroidManifest.xml | sed -e 's/.*"\(.*\)".*/v\1/')
 cp build/outputs/mapping/release/mapping.txt mapping-$VER.txt
 cp release/Dictionary-release.apk Dictionary-$VER.apk
-gpg -a --detach-sign Dictionary-$VER.apk
+cp release/Dictionary-release.aab Dictionary-$VER.aab
+gpg -a --detach-sign Dictionary-$VER.apk Dictionary-$VER.aab
 git tag -u 06D4D9C7 "$VER"
