a5116866d03bb547c9ba30d2f9fb9f81588b5bce
Reimar Döffinger <Reimar.Doeffinger@gmx.de>
2025-07-20 19:05:13 +0200

    Fix up package name in last commit

--- src/com/hughes/android/dictionary/engine/DictionaryInfo.java
+++ src/com/hughes/android/dictionary/engine/DictionaryInfo.java
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package com.hughes.android.dictionary;
+package com.hughes.android.dictionary.engine;
 
 import java.io.Serializable;
 import java.util.ArrayList;
